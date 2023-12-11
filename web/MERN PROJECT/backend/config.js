80e3f9c0c36e25e68ab58e8948adf79a98b4c730
fira flash <flexflash09@gmail.com>
2023-12-11 23:18:11 +0300

    ip updates succesfully

--- web/MERN PROJECT/backend/config.js
+++ web/MERN PROJECT/backend/config.js
@@ -1,4 +1,4 @@
 export const port = 5555;
 
 export const mongoDBURL =
-    'mongodb+srv://firafflash:firaflash@book-store.e4ndk5o.mongodb.net/book_store?retryWrites=true&w=majority'
+    'mongodb+srv://mark:mark@book-store.e4ndk5o.mongodb.net/book-collection?retryWrites=true&w=majority'
