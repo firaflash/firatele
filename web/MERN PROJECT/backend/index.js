ea97186fbda4b91c6e452d21e1a41fb8613d5214
fira flash <flexflash09@gmail.com>
2023-12-11 22:58:36 +0300

    data base connect

--- web/MERN PROJECT/backend/index.js
+++ web/MERN PROJECT/backend/index.js
@@ -1,9 +1,22 @@
-import expess from "express";
-import { port } from "./config.js"
+import express from "express";
+import { port, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
-const app = expess();
-
-app.listen(port, () => {
-    console.log(`App is listening to port: ${port}`);
-
+const app = express();
+app.get('/', (request, Response) => {
+    console.log(request)
+    return Response.status(234).send('welcome To fira flash website');
 });
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('app connected to data base');
+        app.listen(port, () => {
+            console.log(`app is listening to port: ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
